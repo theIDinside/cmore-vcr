43c40f8328fc71d395be397bb232bc004ca5f724
Simon Farre <simon.farre.cx@gmail.com>
2022-12-19 16:39:25 +0100

    Run ffmpeg on all .webm files
    Output matroska (mkv) since it seems to be the only codec that Linux doesn't catastrophically f**k up.

---
+++ run_ffmpeg.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Due to the output from Firefox being so flakey, _or_ due to Linux's atrocious codecs management
+# we run all files through ffmpeg
+
+suffix=".webm"
+
+for segment in $1/*.webm; do
+  ffmpeg -i $1/$segment ${segment/#$suffix}.mkv
+done
