d526bb07100fc668536a07be776d20114e7301f6
Simon Farre <simon.farre.cx@gmail.com>
2022-12-19 16:32:56 +0100

    Grabs sequences of N bytes

--- vcr.js
+++ vcr.js
@@ -1,81 +1,63 @@
-function sequence_time(unit, value) {
-	if(unit != "s" && unit != "ms" && unit != "m") {
-		throw new Error("Time unit should be string values of: 'ms', 's' or 'm', representing milliseconds, seconds and minutes");
-	}
-	return { unit, value };
+const kilobyte = (kb_) => Math.round(kb_ * 1024);
+const megabyte = (mb_) => kilobyte(mb_) * 1024;
+
+function start_recording(stream, minimum_bytes_to_collect) {
+  return new Promise(async (resolve, reject) => {
+    const options = { mimeType: "video/webm;codecs:vp8" };
+    const mr = new MediaRecorder(stream, options);
+    const chunks = [];
+    let total_size = 0;
+    mr.ondataavailable = (e) => {
+      const blob = e.data;
+      total_size += blob.size;
+      console.log(`Blob size: ${blob.size} (${total_size}/${minimum_bytes_to_collect}) of mime-type: ${blob.type}`);
+      chunks.push(blob);
+      if(total_size >= minimum_bytes_to_collect) {
+        if(mr.state == "recording") {
+          mr.stop();
+        }
+      }
+    };
+    mr.start();
+    mr.onstop = () => {
+      resolve(chunks);
+    };
+    mr.onerror = (evt) => reject(evt.name);
+    while(total_size < minimum_bytes_to_collect) {
+      await new Promise((resolve, reject) => {
+        setTimeout(resolve, 2000);
+      }).then(() => {
+        if(mr.state == "recording") {
+          mr.requestData();
+        }
+      });
+    }
+  });
 }
 
-function to_milliseconds(time_length) {
-	if(time_length.hasOwnProperty("unit")) {
-			switch(time_length.unit) {
-			case "ms":
-				return time_length.value;
-				break;
-			case "s":
-				return time_length.value * 1000;
-				break;
-			case "m":
-				return time_length.value * 60 * 1000;
-				break;
-			default:
-				throw new Error("Unknown time unit: " + time_length.unit);
-		}
-	} else {
-		throw new Error(`Parameter Object 'time_length' is not of layout: { "unit": "ms" | "s" | "m", value: number }: ${time_length}`);
-	}
+function grab_sequence(sequence_name, minimum_bytes_to_collect) {
+  console.log(`Grabbing ${minimum_bytes_to_collect} (minimum) bytes`);
+  let name = sequence_name ?? "sequence";
+  let video = document.querySelector("video");
+  return start_recording(video.mozCaptureStream(), minimum_bytes_to_collect).then(data_collected => {
+    let blob = new Blob(data_collected, { type: "video/webm" } );
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.style = "display: none;";
+    a.href = url;
+    a.download = `${name}.webm`;
+    a.click();
+    window.URL.revokeObjectURL(url);
+  });
 }
 
-function wait(delayInMS) {
-  console.log(`waiting for ${delayInMS}`);
-  return new Promise((resolve) => setTimeout(resolve, delayInMS));
-}
-
-function start_recording(stream, time_length) {
-	const ms = to_milliseconds(time_length);
-	const options = { mimeType: "video/webm;codecs:vp8" };
-	const mr = new MediaRecorder(stream, options);
-	const chunks = [];
-	mr.ondataavailable = (e) => chunks.push(e.data);
-	mr.start();
-
-	let stopped = new Promise((resolve, reject) => {
-		mr.onstop = resolve;
-		mr.onerror = (evt) => reject(evt.name);
-	});
-
-	let recorded = wait(ms).then(() => {
-		if(mr.state == "recording") mr.stop();
-	});
-
-	return Promise.all([stopped, recorded])
-	.then(() => chunks);
-}
-
-function grab_sequence(time, sequence_name) {
-	let name = sequence_name ?? "sequence";
-	let video = document.querySelector("video");
-	return start_recording(video.mozCaptureStream(), time).then(data_collected => {
-		let blob = new Blob(data_collected, { type: "video/webm" } );
-		const url = URL.createObjectURL(blob);
-		const a = document.createElement("a");
-		a.style = "display: none;";
-		a.href = url;
-		a.download = `${name}.webm`;
-		a.click();
-		window.URL.revokeObjectURL(url);
-	});
-}
-
-function grab_sequences(count) {
-  
-}
-
-async function test() {
-	const span = sequence_time("s", 5);
-	let idx = 1;
-	const sequences = [span, span, span]
-	for(const s of sequences) {
-		await grab_sequence(span, `test-${idx}`);
-		idx+=1;
-	}
+/**
+ * Grab `sequences` of sequences of size `megabytes`
+ * @param {number} sequences
+ * @param {number} megabytes
+ */
+async function grab_stream(sequences, megabytes) {
+  for(let idx = 0; idx < sequences; idx += 1) {
+    await grab_sequence(`test-${idx}`, megabyte(megabytes));
+  }
 }
